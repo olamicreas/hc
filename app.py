228f22a03647c5f26f5db95989486a164ee7c419
olamicreas <70097198+olamicreas@users.noreply.github.com>
2024-10-18 10:50:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = 'mail.satoriselfcare.com'  # cPanel mail server address
 app.config['MAIL_PORT'] = 465  # Port for SSL (or 587 for TLS)
-app.config['MAIL_USERNAME'] = os.environ['email']
+app.config['MAIL_USERNAME'] = os.environ['smtp_email']
 app.config['MAIL_PASSWORD'] = os.environ['smtp_password']  # The password for booking@satoriselfcare.com
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 app.config['MAIL_USE_TLS'] = False  # Use SSL instead of TLS
